5404467568003acb30bbfacb119665704422f6a3
Luis De Andrade <luisdeandrade92@gmail.com>
2023-04-18 12:24:36 +0100

    nft dinotoken

--- src/app/nft-connect/nft-connect.component.ts
+++ src/app/nft-connect/nft-connect.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ethers } from 'ethers';
 import { environment } from 'src/environments/environment.development';
-import abi from "../../contracts/LuisCoin.json";
+import abi from "../../contracts/DinoToken.json";
 
 @Component({
   selector: 'app-nft-connect',
@@ -12,14 +12,11 @@ export class NftConnectComponent implements OnInit {
   title = 'Ethereum-101-Angular';
 
   isWalletConnected = false;
-  isOwner = false;
   contractABI = abi.abi;
-  contractAddress = environment.contract_address_coin;
-  user: any = { address: null, symbol: null, owner: null, supply: null, balance: null };
-  transferAmount!: string;
-  mintAmount!: string;
-  burnAmount!: string;
-  transferAddress!: string;
+  contractAddress = environment.contract_address_token;
+  user: any = { };
+  mintUri!: string;
+  mintAddress!: string;
 
   async ngOnInit(): Promise<void> {}
 
@@ -33,6 +30,7 @@ export class NftConnectComponent implements OnInit {
         this.user.address = account;
         this.user.balance = ethers.utils.formatEther(await windowEth.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']}));
         console.log("Account Connected: ", account);
+        await this.getNftInfo();
       } else {
         alert("Please install a MetaMask wallet to use our store.");
         console.log("No Metamask detected");
@@ -45,7 +43,49 @@ export class NftConnectComponent implements OnInit {
   async onDisConnect(): Promise<void> {
     window.localStorage.clear();
     this.isWalletConnected = false;
-    this.user.address = null;
-    this.user.balance = null;
+    this.user = { };
+  }
+
+  async getNftInfo(): Promise<void> {
+    try {
+      const windowEth = window as any;
+      if (windowEth.ethereum) {
+        const provider = new ethers.providers.Web3Provider(windowEth.ethereum);
+        const signer = await provider.getSigner();
+        const DTKContract = new ethers.Contract(this.contractAddress, this.contractABI, signer) as any;
+
+        this.user.name = await DTKContract.name();
+        this.user.symbol = await DTKContract.symbol();
+        this.user.owner = await DTKContract.owner();
+        this.user.balanceNft = await DTKContract.balanceOf(this.user.address);
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        alert("Please install a MetaMask wallet to use our market.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async MintNft(): Promise<void> {
+    try {
+      const windowEth = window as any;
+      if (windowEth.ethereum) {
+        const provider = new ethers.providers.Web3Provider(windowEth.ethereum);
+        const signer = await provider.getSigner();
+        const DTKContract = new ethers.Contract(this.contractAddress, this.contractABI, signer) as any;
+
+        const txn = await DTKContract.safeMint(this.mintAddress, this.mintUri);
+        console.log("Minting nft...");
+        await txn.wait();
+        console.log("Nft Minted", txn.hash);
+        await this.getNftInfo();
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        alert("Please install a MetaMask wallet to use our market.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   contract_address_vgs: "0x367252EbEF267E0bC156bF9b518BBB9db38F1A7B",
-  contract_address_coin: "0x364683D87f2b9C71Aaf412A09b21079cffb06191"
+  contract_address_coin: "0x364683D87f2b9C71Aaf412A09b21079cffb06191",
+  contract_address_token: "0x17e3871c6993be65dE9E4431EcC3a1C1AA8E8eB7"
 };
